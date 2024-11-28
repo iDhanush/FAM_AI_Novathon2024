9664bbf84fb8f4b3b6e4041234659d57676d9a10
iDhanush <idhanushm@gmail.com>
2024-11-28 20:13:27 +0530

    profile schema update

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,16 +1,17 @@
-import datetime
+from typing import Union
 
 from pydantic import BaseModel
 
 
 class AccountData(BaseModel):
     address: str
-
+class ProfileUID(BaseModel):
+    prfid: str
 
 class ProfileData(BaseModel):
-    address: str
+    address:  Union[str, None] = None
+    prfid: Union[str, None] = None
     name: str
     dob: str
     gender: str
     blood: str
-
