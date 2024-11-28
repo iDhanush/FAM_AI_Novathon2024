4db792eacb2e5ec950f7956f1b56c7dcac18dcfb
Abhinav K E <109943270+Abhinav-K-E@users.noreply.github.com>
2024-11-28 18:28:53 +0530

    fam ui added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import FamilyProfile from "./pages/Profiles/FamilyProfile.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path="/fam" element={<FamilyProfile />} />
+      <Route path="/profile" element={<App />} />
     </Routes>
   </BrowserRouter>
 );
